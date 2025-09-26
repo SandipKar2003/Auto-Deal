42c4b6f26f5d3192195757558c8b753bbc2b13dd
Sandip Kar <sandip2003kar@gmail.com>
2025-09-26 15:38:30 +0530

    Prepare for deployment

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL ="mysql+pymysql://caruser:carpassword@localhost:3306/car_rental"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SQLALCHEMY_DATABASE_URL =os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
